60873ba5f3ec1ec09257138c3e6f94833649ef92
Peter Bacinsky <peter@bacinsky.sk>
2014-10-16 22:03:05 +0200

    Updated Gruntfile, require shelljs to resolve node path

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,10 @@
+var root;
+if (undefined === process.env.NODE_PATH) {
+    require('shelljs/global');
+    process.env.NODE_PATH = root = exec('npm root -g', {silent: true}).output.trim() + '/';
+}
 module.exports = function(grunt) {
     grunt.initConfig((function () {
-        return require('webino-devkit');
+        return require(root + 'webino-devkit');
     })().config(grunt, ['zend', 'module', 'auth']));
 };
