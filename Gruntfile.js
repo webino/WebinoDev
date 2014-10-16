18edd05631f430f8ff717bcd2157394d9bfc4bf7
Peter Bacinsky <peter@bacinsky.sk>
2014-10-16 17:46:29 +0200

    Updated Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,5 @@
 module.exports = function(grunt) {
-    /**
-     * We want a global devkit
-     * Export env NODE_PATH otherwise we use default.
-     */
-    var node_path = process.env.NODE_PATH ? process.env.NODE_PATH : '/usr/local/lib/node_modules';
-    /**
-     * Initialize grunt with webino-devkit config
-     */
-    grunt.config.init((function () {
-        return require(node_path + '/webino-devkit');
+    grunt.initConfig((function () {
+        return require('webino-devkit');
     })().config(grunt, ['zend', 'module', 'auth']));
 };
