f28a421038c38ad34afa0accd23fd2242f5cc4e7
Peter Bacinsky <peter@bacinsky.sk>
2014-10-18 03:33:10 +0200

    Updated functions.php, added new functions

--- inc/functions.php
+++ inc/functions.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Webino (http://webino.sk/)
+ *
+ * @link        https://github.com/webino/WebinoDev/ for the canonical source repository
+ * @copyright   Copyright (c) 2014 Webino, s. r. o. (http://webino.sk/)
+ * @license     BSD-3-Clause
+ */
 
 /**
  * Alias for var_dump()
@@ -10,11 +17,53 @@ function d($subject) {
 }
 
 /**
- * Diyng dump
+ * Diyng var_dump()
  *
  * @param mixed $subject
  */
 function dd($subject) {
     var_dump($subject);
     exit;
+}
+
+/**
+ * Alias for print_r() scream
+ *
+ * @param mixed $subject
+ * @param bool $return
+ */
+function p($subject) {
+    print_r($subject, $return);
+}
+
+/**
+ * Dying print_r() scream
+ *
+ * @param mixed $subject
+ * @param bool $return
+ */
+function pd($subject) {
+    print_r($subject, $return);
+}
+
+/**
+ * Alias for print_r() return
+ *
+ * @param mixed $subject
+ * @param bool $return
+ */
+function pr($subject) {
+    return print_r($subject, true);
+}
+
+/**
+ * Web debugger break point
+ *
+ * Sometimes is useful by throwing an exception to check a backtrace.
+ *
+ * @link https://github.com/webino/WebinoDebug Web debugger
+ * @param string $msg
+ */
+function e($msg = '') {
+    throw new \WebinoDev\Exception\DevelopmentException($msg);
 }
