d64ffa9f7e53977e4937e2c7f1cdee554572c431
Peter Bacinsky <peter@bacinsky.sk>
2014-10-18 03:34:35 +0200

    Updated testing application config

--- tests/resources/config/application.config.php
+++ tests/resources/config/application.config.php
@@ -1,40 +1,32 @@
 <?php
 /**
- * Webino (http://webino.sk)
+ * Webino (http://webino.sk/)
  *
- * @link        https://bitbucket.org/webino/webinodev for the canonical source repository
- * @copyright   Copyright (c) 2014 Webino, s. r. o. (http://webino.sk)
- * @author      Peter Bacinsky <peter@bacinsky.sk>
- * @license     proprietary
+ * @link        https://github.com/webino/WebinoDev/ for the canonical source repository
+ * @copyright   Copyright (c) 2014 Webino, s. r. o. (http://webino.sk/)
+ * @license     BSD-3-Clause
  */
 
 namespace WebinoDev;
 
 /**
- * Test application config
+ * Testing application config
  */
 return [
     'modules' => [
         'Application',
-        // Module is added as last
-        // for test purposes only
         'WebinoDev',
     ],
     'module_listener_options' => [
-        'cache_dir'                => 'data/cache',
-        'config_cache_enabled'     => false,
-        'module_map_cache_enabled' => false,
-        'check_dependencies'       => false,
-
-        'module_paths' => [
-            'module',
-            'vendor',
-        ],
         'config_glob_paths' => [
             'config/autoload/{,*.}{global,local}.php',
         ],
         'config_static_paths' => [
             __DIR__ . '/module.config.php',
+        ],
+        'module_paths' => [
+            'module',
+            'vendor',
         ],
     ],
 ];
